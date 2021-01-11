34a5e630f675348b19b295cbf6a148959923cea7
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-11 00:09:24 -0600

    [Makefile] commit

--- stage-2/etc/profile.d/support.sh
+++ stage-2/etc/profile.d/support.sh
@@ -4,13 +4,15 @@ export SOCKS_PROXY
 /usr/local/bin/support-wrapper &
 P="$!"
 support_exit () {
-        [ $# -eq 1 ]
         e="$?"
+	set -eu
+        [ $# -eq 1 ]
+	kill -0 "$1"
         kill    "$1"
         wait    "$1" || :
         exit    "$e"
 }
-kill -0 "$P"
+kill -0 "$P" || exit "$?"
 # shellcheck disable=SC2064
 trap "support_exit $P" 0
 unset P
