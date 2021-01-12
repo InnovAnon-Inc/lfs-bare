3ae41d1bf90f00771b6603af859e91f37bc0f5e6
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-12 16:48:49 -0600

    squashing

--- stage-4/usr/local/bin/squash.sh
+++ stage-4/usr/local/bin/squash.sh
@@ -4,16 +4,22 @@ set -euvxo pipefail
 (( ! $#  ))
 cd          /
 touch       /final.tar
-tar pcf     /final.tar \
-  --exclude=/final.tar \
-  --exclude=/dev       \
-  --exclude=/media     \
-  --exclude=/mnt       \
-  --exclude=/proc      \
-  --exclude=/run       \
-  --exclude=/sys       \
-  --exclude=/tmp       \
-  --exclude=/var/tmp   \
-  --exclude=/var/cache /
+tar pcf     /final.tar                  \
+  --exclude=/final.tar                  \
+  --exclude=/dev                        \
+ '--exclude=/home/*/.gvfs'              \
+ '--exclude=/home/*/.cache'             \
+ '--exclude=/home/*/.local/share/Trash' \
+  --exclude=/media                      \
+  --exclude=/mnt                        \
+  --exclude=/proc                       \
+  --exclude=/run                        \
+  --exclude=/usr/src                    \
+  --exclude=/sys                        \
+  --exclude=/tmp                        \
+  '--exclude=/var/cache/*'              \
+  '--exclude=/var/log/*'                \
+  '--exclude=/var/tmp/*'                \
+  /
 mv -v {,/tmp}/final.tar
 
