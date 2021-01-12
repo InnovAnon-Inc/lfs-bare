d9a95ca00efb83bfb31f351394368473d8717cae
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-01-12 17:30:25 -0600

    squashing

--- stage-4/usr/local/bin/squash.sh
+++ stage-4/usr/local/bin/squash.sh
@@ -22,7 +22,7 @@ tar pcf     /final.tar                  \
   '--exclude=/var/log/*'                \
   '--exclude=/var/tmp/*'                \
   --exclude=nohup.out                   \
-  bin etc home lib lib32 lib64 libx32   \
-  usr root sbin srv opt var
+  {,s}bin etc home lib{,64} opt usr     \
+  root srv var
 mv -v {,/tmp}/final.tar
 
